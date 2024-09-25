7d7d11f5abac172fcf9d873eddba09578a40f11a
Komorebi <2391587114@qq.com>
2024-09-25 15:30:12 +0800

    ✨ feat: 初步搭建一个可切换element组件的明暗主题的demo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+/*
+ * @Author: Komorebi
+ * @Date: 2024-09-23 15:08:24
+ * @LastEditors: Komorebi
+ * @LastEditTime: 2024-09-25 15:13:05
+ */
+import { createApp } from "vue";
+import App from "./App.vue";
+// 注册svg图标
+import "virtual:svg-icons-register";
+// 导入UnoCss
+import "uno.css";
+// 引入element内组件的暗合主题颜色
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");

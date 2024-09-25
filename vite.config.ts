c74fa4220467ef29311475a4ede52bee63556a54
Komorebi <2391587114@qq.com>
2024-09-25 11:53:05 +0800

    🐎 ci: 调整vite.config.js下的配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: Komorebi
  * @Date: 2024-09-23 15:08:24
  * @LastEditors: Komorebi
- * @LastEditTime: 2024-09-24 17:04:29
+ * @LastEditTime: 2024-09-25 11:51:45
  */
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -11,6 +11,21 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // 组件自动导入
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
+// node 内置
+import { resolve } from "path";
+// css 自动添加前缀
+import autoprefixer from "autoprefixer";
+// 引入svg插件
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+// 引入UnoCss
+import UnoCss from "unocss/vite";
+/**
+ * 使用unocss的预设
+ * 这些预设可在html中
+ * 添加uno独有的样式属性
+ * eg. <div hover="color-white bg-orange"></div>
+ */
+import { presetUno, presetAttributify } from "unocss";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -21,6 +36,7 @@ export default defineConfig(({ mode }) => {
   // console.log("🚀 ~ defineConfig ~ Env:", Env);
 
   return {
+    /* 共享选项 */
     plugins: [
       vue(),
       AutoImport({
@@ -42,7 +58,82 @@ export default defineConfig(({ mode }) => {
         extensions: ["vue", "tsx"],
         dts: "types/components.d.ts",
       }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [resolve(process.cwd(), "src/icons")],
+        // 指定symbolId格式
+        symbolId: "icon-[dir]-[name]",
+      }),
+      // UnoCss 添加配置
+      UnoCss({
+        // 手动配置的预设
+        rules: [
+          // eg: ["red", { color: "red" }],
+          [/^font-size-(\d+)px$/, ([, d]) => ({ "font-size": `${d}px` })],
+        ],
+        // 自定义组合样式
+        // shortcuts: {
+        //   "app-header-icon":
+        //     "cursor-pointer hover-bg-#f6f6f6 color-#3e3e3e p-t-14.5px p-r-10px p-b-14.5px p-l-10px",
+        // },
+        // 使用预设
+        presets: [presetUno(), presetAttributify()],
+      }),
     ],
+    resolve: {
+      // 以下文件在import时可以不写后缀
+      // extensions: [".js", ".ts", ".vue"],
+      alias: [
+        // @/xxx => src/xxx
+        {
+          find: /@\//,
+          replacement: pathResolve("src") + "/",
+        },
+        // #/xxx => types/xxx
+        {
+          find: /#\//,
+          replacement: pathResolve("types") + "/",
+        },
+        /**
+         * vue-i18n控制台会报黄色警告,不影响使用
+         * 设置别名就不会显示警告
+         */
+        {
+          find: "vue-i18n",
+          replacement: "vue-i18n/dist/vue-i18n.cjs.js",
+        },
+      ],
+    },
+    css: {
+      // vite 中已集成postcss
+      postcss: {
+        plugins: [
+          autoprefixer({
+            overrideBrowserslist: [
+              "Android 4.1",
+              "iOS 7.1",
+              "Chrome > 31",
+              "ff > 31",
+              "> 1%",
+            ],
+            grid: true,
+          }),
+        ],
+      },
+      // css预处理器
+      preprocessorOptions: {
+        // scss 全局变量
+        scss: {
+          /**
+           * !导入的文件路径必须加上分号, 不然报错
+           * !不能使用@import来导入, sass团队将废弃该语法
+           */
+          additionalData: `
+            @use "./src/styles/variable.scss" as *;
+          `,
+        },
+      },
+    },
 
     /* 开发服务器选项 */
     server: {
@@ -97,3 +188,8 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
+
+// 返回相对于当前的工作目录
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), ".", dir);
+}

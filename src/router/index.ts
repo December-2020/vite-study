a2b597230a97fffc868e629fe2289061ff642cf0
Komorebi <2391587114@qq.com>
2024-09-27 14:32:18 +0800

    ✨ feat: 文件动态引入类型提示整改

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,22 +2,30 @@
  * @Author: Komorebi
  * @Date: 2024-09-26 11:26:00
  * @LastEditors: Komorebi
- * @LastEditTime: 2024-09-27 11:26:27
+ * @LastEditTime: 2024-09-27 14:23:54
  */
 import type { App } from "vue";
 import type { RouteRecordRaw } from "vue-router";
 import type { AppRouteRecordRaw } from "#/route";
+import type { FileImport } from "#/global";
 
 import { createRouter, createWebHistory } from "vue-router";
-import { WHITE_NAME_LIST, constantRoutes } from "./constant";
+import { constantRoutes } from "./modules/constant";
+// import { WHITE_NAME_LIST, constantRoutes } from "./constant";
 
 // 异步路由(需要权限之类的)
 const asyncRoutes: AppRouteRecordRaw[] = [];
-const modules = import.meta.glob("./modules/*.ts", { eager: true });
+/**
+ * * 导入所有 非constant 的相关路由
+ * ? 这里的 FileImport 是不该存在的
+ * ? 若后续有更好的写法再改
+ */
+const modules: FileImport = import.meta.glob("./modules/!(constant).ts", {
+  eager: true,
+});
 Object.keys(modules).forEach((key) => {
   //// @ts-ignore
   const module = modules[key].default || {};
-  console.log("🚀 ~ Object.keys ~ modules[key]:", modules[key]);
   const moduleList = Array.isArray(module) ? module : [module];
   asyncRoutes.push(...moduleList);
 });

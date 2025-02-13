3feab724d8aa541e00631bfb3c455f4ac3b783db
Komorebi <2391587114@qq.com>
2025-02-13 17:15:00 +0800

    ↩ revert: 基础switch组件回退, 使用之前的方法vscode中确实不会报错, 但页面内的插槽不生效

--- src/components/composition/BaseSwitch.vue
+++ src/components/composition/BaseSwitch.vue
@@ -2,18 +2,17 @@
  * @Author: Komorebi
  * @Date: 2024-10-08 15:15:18
  * @LastEditors: Komorebi
- * @LastEditTime: 2025-02-13 15:31:58
+ * @LastEditTime: 2025-02-13 16:49:20
 -->
 <template>
   <el-switch
     class="base-switch"
-    v-model="model"
     v-bind="props"
+    v-model="model"
     @change="emit('change')"
   >
-    <!-- ref="switchRef" -->
     <template
-      v-for="(_, slotName) in slotNames"
+      v-for="(_, slotName) in $slots"
       :key="slotName"
       v-slot:[slotName]="slotProps"
     >
@@ -23,38 +22,14 @@
 </template>
 
 <script setup lang="ts">
-import type { Component } from "vue";
-import type { ComponentSize } from "element-plus";
+import type { SwitchProps } from "element-plus";
 
-import { getCurrentInstance } from "vue";
-
-interface Props {
-  disabled?: boolean;
-  loading?: boolean;
-  size?: ComponentSize;
-  width?: number | string;
-  inlinePrompt?: boolean;
-  activeIcon?: string | Component;
-  inactiveIcon?: string | Component;
-  activeActionIcon?: string | Component;
-  inactiveActionIcon?: string | Component;
-  activeText?: string;
-  inactiveText?: string;
-  activeValue?: boolean | string | number;
-  inactiveValue?: boolean | string | number;
-  name?: string;
-  validateEvent?: boolean;
-  //   beforeChange?: boolean | (() => Promise<boolean>);
-  id?: string;
-  tabindex?: string | number;
-  ariaLabel?: string;
-}
-
-const instance = getCurrentInstance();
-const slotNames = Object.keys(instance?.slots || {}) as string[];
+type Props = Partial<SwitchProps>;
 
 const props = withDefaults(defineProps<Props>(), {
-  //   size:"small"
+  activeValue: true,
+  inactiveValue: false,
+  validateEvent: true,
 });
 
 const model = defineModel({
@@ -62,22 +37,6 @@ const model = defineModel({
 });
 
 const emit = defineEmits(["change"]);
-
-// 获取子组件的 ref
-// const switchRef = ref();
-// defineExpose(
-//   new Proxy(
-//     {},
-//     {
-//       get(_target, prop) {
-//         return switchRef.value?.[prop];
-//       },
-//       has(_target, prop) {
-//         return prop in switchRef.value;
-//       },
-//     }
-//   )
-// );
 </script>
 
 <style scoped lang="scss"></style>
--- src/views/example/comp/dialog/components/Modal1.vue
+++ src/views/example/comp/dialog/components/Modal1.vue
@@ -2,9 +2,30 @@
   <div class="wrapper">
     <h2>Modal1测试弹窗</h2>
     <BaseButton @click="modal1_1 = true">显示</BaseButton>
+
+    <div>
+      <BaseSwitch v-model="switchVal">
+        <template #active-action>
+          <span class="custom-active-action">T</span>
+        </template>
+        <template #inactive-action>
+          <span class="custom-inactive-action">F</span>
+        </template>
+      </BaseSwitch>
+      <ElSwitch
+        v-model="switchVa2"
+        :active-action-icon="View"
+        :inactive-action-icon="Hide"
+      />
+    </div>
+
     <h3 v-for="(item, index) in 20" :key="index">{{ item }}</h3>
 
-    <BaseDialog title="测试弹窗1-1" v-model="modal1_1" @close="modal1_1 = false">
+    <BaseDialog
+      title="测试弹窗1-1"
+      v-model="modal1_1"
+      @close="modal1_1 = false"
+    >
       <Modal1_1 />
     </BaseDialog>
   </div>
@@ -12,8 +33,11 @@
 
 <script setup lang="ts">
 import Modal1_1 from "./Modal1_1.vue";
+import { Hide, View } from '@element-plus/icons-vue'
 
 const modal1_1 = ref(false);
+const switchVal = ref(true);
+const switchVa2 = ref(false);
 </script>
 
 <style scoped lang="scss"></style>

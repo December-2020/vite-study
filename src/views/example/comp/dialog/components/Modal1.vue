c0d8fcaaea273f98f4a6dae1771600b1584e96f3
Komorebi <2391587114@qq.com>
2025-02-14 15:40:54 +0800

    🧪 test: 删除测试的switch相关的代码

--- src/views/example/comp/dialog/components/Modal1.vue
+++ src/views/example/comp/dialog/components/Modal1.vue
@@ -3,22 +3,6 @@
     <h2>Modal1测试弹窗</h2>
     <BaseButton @click="modal1_1 = true">显示</BaseButton>
 
-    <div>
-      <BaseSwitch v-model="switchVal">
-        <template #active-action>
-          <span class="custom-active-action">T</span>
-        </template>
-        <template #inactive-action>
-          <span class="custom-inactive-action">F</span>
-        </template>
-      </BaseSwitch>
-      <ElSwitch
-        v-model="switchVa2"
-        :active-action-icon="View"
-        :inactive-action-icon="Hide"
-      />
-    </div>
-
     <h3 v-for="(item, index) in 20" :key="index">{{ item }}</h3>
 
     <BaseDialog
@@ -33,11 +17,8 @@
 
 <script setup lang="ts">
 import Modal1_1 from "./Modal1_1.vue";
-import { Hide, View } from '@element-plus/icons-vue'
 
 const modal1_1 = ref(false);
-const switchVal = ref(true);
-const switchVa2 = ref(false);
 </script>
 
 <style scoped lang="scss"></style>

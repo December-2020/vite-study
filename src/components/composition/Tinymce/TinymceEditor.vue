da2996aaed2766a713ad985b8cb8e3d4f359506f
Komorebi <2391587114@qq.com>
2025-05-24 17:16:40 +0800

    ✨ feat: 继续搭建TinyMCE富文本编辑器(5%)

--- src/components/composition/Tinymce/TinymceEditor.vue
+++ src/components/composition/Tinymce/TinymceEditor.vue
@@ -2,10 +2,10 @@
  * @Author: Komorebi
  * @Date: 2025-05-09 15:34:33
  * @LastEditors: Komorebi
- * @LastEditTime: 2025-05-16 13:51:02
+ * @LastEditTime: 2025-05-24 16:22:04
 -->
 <template>
-  <div class="editor">
+  <div class="editor-wrap">
     <textarea :id="tinymceId" ref="elRef" />
   </div>
 </template>
@@ -13,9 +13,9 @@
 <!-- 菜单栏 工具栏 内容栏 共同组合成一个富文本 -->
 
 <script setup lang="ts">
-import type { Editor, RawEditorOptions } from "tinymce";
+import type { Editor, RawEditorOptions, EditorEvent } from "tinymce";
 
-import tinymce from "tinymce/tinymce";
+// import tinymce from "tinymce/tinymce";
 import { buildShortUUID } from "@/utils/uuid";
 import { plugins as defaultPlugins, toolbar as defaultToolbar } from "./config";
 
@@ -50,7 +50,7 @@ const props = defineProps({
   },
 });
 
-/** 
+/**
  * 仅在 3.4+ 中可用
  * 声明 "modelValue" prop，由父组件通过 v-model 使用
  */
@@ -61,6 +61,73 @@ const tinymceId = ref<string>(buildShortUUID("tiny-vue"));
 // textarea的ref
 const elRef = ref<HTMLElement | null>(null);
 const editorRef = ref<Editor | null>(null);
+
+// 计算组件宽度
+const editorWidth = computed(() => {
+  const width = props.width;
+  if (typeof width === "number") {
+    return `${width}px`;
+  }
+  return width;
+});
+/**
+ * TODO: 计算主题色
+ */
+/**
+ * TODO: 国际化
+ */
+/**
+ * TODO: 初始化配置
+ * @see http://tinymce.ax-z.cn/configure/integration-and-setup.php
+ */
+const initOptions = computed((): RawEditorOptions => {
+  const { height, options, toolbar, plugins } = props;
+  return {
+    selector: `#${unref(tinymceId)}`,
+    height,
+    toolbar,
+    plugins,
+    /**
+     * 默认配置
+     */
+    // table tools help
+    menubar: "file edit insert view format",
+    // 隐藏右下角技术支持
+    branding: false,
+    // 自动获得焦点
+    auto_focus: true,
+    // 禁用链接对话框中的链接标题输入字段
+    link_title: false,
+    // 关闭图像、表格或媒体对象的大小调整手柄。
+    // 默认情况下，此选项处于启用状态，允许您调整表格和图像的大小。
+    object_resizing: false,
+    /**
+     * 覆盖默认配置
+     */
+    ...options,
+    // 初始化前执行
+    setup: function (editor) {
+      editorRef.value = editor;
+      console.log("ID为: " + editor.id + " 的编辑器即将初始化.");
+      editor.on("init", (e) => setupEditor(e));
+    },
+  };
+});
+
+// 组件初始化
+// 组件初始化完成
+function setupEditor(e: EditorEvent<any>) {
+  const editor = unref(editorRef);
+  if(!editor) return;
+
+  const value = model.value || "";
+  editor.setContent(value as string);
+}
+
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.editor-wrap {
+  width: v-bind("editorWidth");
+}
+</style>

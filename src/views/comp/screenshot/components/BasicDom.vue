6c236765f4a21ac8f1fbe5c0db8e014e604bbfd8
Komorebi <2391587114@qq.com>
2025-07-04 16:16:15 +0800

    ✨ feat: snapDom新增一个基础示例, 功能待完善

---
+++ src/views/comp/screenshot/components/BasicDom.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Author: Komorebi
+ * @Date: 2025-07-04 14:45:42
+ * @LastEditors: Komorebi
+ * @LastEditTime: 2025-07-04 16:11:03
+-->
+<template>
+  <div class="wrapper">
+    <span>
+      <b>Hello, <span class="highlight">This is a base demo</span>! </b>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 32px 40px;
+  font-weight: bold;
+  border-radius: 12px;
+  background: linear-gradient(120deg, #f8fbff 60%, #eaf3fa 100%);
+  box-shadow: 0 4px 16px 0 rgba(30, 42, 80, 0.08);
+  position: relative;
+  border: none;
+  font-size: large;
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 10px;
+    height: 100%;
+    border-radius: 14px 0 0 14px;
+    background: linear-gradient(180deg, #ff6a3d 0%, #ffb86c 100%);
+    box-shadow: 2px 0 8px 0 rgba(255, 106, 61, 0.15);
+  }
+  .highlight {
+    font-weight: bold;
+  }
+  span {
+    @include font_color("content-bg-color");
+  }
+}
+</style>
--- src/views/comp/screenshot/snapDom/index.vue
+++ src/views/comp/screenshot/snapDom/index.vue
@@ -2,19 +2,19 @@
  * @Author: Komorebi
  * @Date: 2025-07-04 11:16:32
  * @LastEditors: Komorebi
- * @LastEditTime: 2025-07-04 11:16:49
+ * @LastEditTime: 2025-07-04 14:20:19
 -->
 <template>
-    <div class="wrapper">
-        使用snapDOM进行截图
-    </div>
+  <div class="wrapper">使用snapDOM进行截图
+    <BasicDom />
+  </div>
 </template>
 
 <script setup lang="ts">
-defineOptions({ name: "snapDom" });
+import BasicDom from "../components/BasicDom.vue";
+import { snapdom } from "@zumer/snapdom";
 
+defineOptions({ name: "snapDom" });
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>

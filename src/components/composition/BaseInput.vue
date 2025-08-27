ef194d0504db90a5a74120b883fb6e1cf44a56a0
Komorebi <2391587114@qq.com>
2025-08-27 17:21:57 +0800

    🦄 refactor: 再次重新梳理input组件所需的功能, 已完成20%

--- src/components/composition/BaseInput.vue
+++ src/components/composition/BaseInput.vue
@@ -2,17 +2,10 @@
  * @Author: Komorebi
  * @Date: 2024-09-26 11:28:51
  * @LastEditors: Komorebi
- * @LastEditTime: 2025-08-22 17:00:19
+ * @LastEditTime: 2025-08-27 17:14:59
 -->
 <template>
-  <ElInput
-    v-bind="{ ...props, ...$attrs }"
-    v-model="model"
-    ref="inputRef"
-    class="base-input"
-    v-slots="$slots"
-  />
-  <!-- @keydown="onKeydown" -->
+  <ElInput class="base-input" @keydown="handleKeydown"></ElInput>
 </template>
 
 <script setup lang="ts">
@@ -21,82 +14,76 @@ import type {
   InputProps as ElInputProps,
   InputInstance as ElInputInstance,
 } from "element-plus";
-// ts中使用i18n
+
+import { mergeProps } from "vue";
 import i18n from "@/locales";
 
 interface InputProps extends ExtractPublicPropTypes<ElInputProps> {
-  // 自定义组件宽度
   width?: number | string;
-  // 阻止输入框的键盘事件
   preventKeyboardEvent?: boolean;
-  // 键盘事件
   keyboardList?: KeyboardEvent["key"][];
 }
 
 /**
+ * * props中使用i18n
  * * defineProps不能引用setup内的变量。
  * * const { t } = useI18n(); 这里的t也是属于变量
  * * 所以不能直接 placeholder: t("xxx")
  */
 const props = withDefaults(defineProps<InputProps>(), {
   placeholder: i18n.global.t(`Components.inputPlaceholder`),
+  // 自定义组件宽度
   width: 198,
+  // 阻止输入框的键盘事件
   preventKeyboardEvent: false,
+  // 需要阻止的键盘事件列表
   keyboardList: () => [],
 });
-// 声明 emit 类型
-const emit = defineEmits<{
-  (e: "keydown", event: KeyboardEvent): void;
-  // 可补充其他事件，如输入变化等
-}>();
+/**
+ *
+ */
+const emit = defineEmits(["keydown"]);
 
 /**
- * * Vue3.4以上才能使用 defineModel
+ * 计算输入框的宽度
  */
-const [model, modifiers] = defineModel({
-  /**
-   * ! 如果为 defineModel prop 设置了一个 default 值
-   * ! 且父组件没有为该 prop 提供任何值
-   * ! 会导致父组件与子组件之间不同步。
-   */
-  default: "",
-  set(value) {
-    // nonnegative 非负整数
-    if (modifiers.nonnegative) {
-      return value.replace(/\D/g, "");
+const inputWidth = computed(() => {
+  const width = props.width;
+  if (typeof width === "number") {
+    return `${width}px`;
+  }
+  if (typeof width === "string") {
+    let num = Number(width);
+    if (!isNaN(num) && num > 0) {
+      return `${width}px`;
     }
-    // decimal 小数
-    if (modifiers.decimal) {
-      // 清除数字和小数点和负号以外的字符
-      let val = value.replace(/[^\d.-]/g, "");
-      // 只保留第一个负号
-      val = val
-        .replace(/^-/, "$#$") // 仅以负号开头,才用 "$#$" 进行代替
-        .replace(/-/g, "") // 清除其他负号
-        .replace("$#$", "-"); // 换回来
-      // 不能以小数点开头
-      // value = value.replace(/^\./, "");
-      // // 只保留第一个小数点
-      // value = value
-      //   .replace(".", "$#$") // 第一个小数点以 "$#$" 进行代替
-      //   .replace(/\./g, "") // 清除其他小数点
-      //   .replace("$#$", "."); // 换回来
-      // return value;
-      // 限制小数点只能有一个，且不能在开头（除非前面有负号）
-      val = val.replace(/^\./, "").replace(/(\.\d*)\./g, "$1");
-      // 移除末尾多余的小数点（如 "123." → "123"）
-      return val.replace(/\.$/, "");
-    }
-    return value;
-  },
+    // px % rem em 'auto'
+    return width;
+  }
 });
-
 /**
- * 获取子组件的 ref
- * * 必须通过defineExpose 暴露子组件
- * * 才能在父组件中通过ref调用
+ * 阻止的键盘事件
  */
-const inputRef = ref<ElInputInstance | null>(null);
+const handleKeydown = (e: KeyboardEvent | Event) => {
+  const { preventKeyboardEvent, keyboardList } = props;
+  if (
+    e instanceof KeyboardEvent &&
+    preventKeyboardEvent &&
+    keyboardList.includes(e.key)
+  ) {
+    e.preventDefault();
+    emit("keydown", e);
+  }
+};
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.base-input {
+  width: v-bind("inputWidth");
+  :deep(.el-input__inner) {
+    &[type="password"] {
+      letter-spacing: -7px;
+    }
+  }
+}
+</style>
--- src/views/test/index1.vue
+++ src/views/test/index1.vue
@@ -1,13 +1,20 @@
+<!--
+ * @Author: Komorebi
+ * @Date: 2024-10-12 09:33:16
+ * @LastEditors: Komorebi
+ * @LastEditTime: 2025-08-27 14:32:19
+-->
 <template>
   <div class="wrapper">
     测试页面11111111111
 
     <div>
       <BaseInput
-        width="400"
+        width="300"
         clearable
         v-model="testIptVal"
         @clear="handleClear"
+      
       >
         <template #prepend>Http://</template>
         <template #append>.com</template>

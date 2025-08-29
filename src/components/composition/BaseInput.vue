fbf6b3ea232abb20a377c23f5164f0a3c589c6b0
Komorebi <2391587114@qq.com>
2025-08-29 17:21:18 +0800

    🦄 refactor: 基础input组件 大致所需的功能均已实现, 代码待优化

--- src/components/composition/BaseInput.vue
+++ src/components/composition/BaseInput.vue
@@ -2,14 +2,19 @@
  * @Author: Komorebi
  * @Date: 2024-09-26 11:28:51
  * @LastEditors: Komorebi
- * @LastEditTime: 2025-08-28 17:13:54
+ * @LastEditTime: 2025-08-29 17:05:02
 -->
 <template>
   <ElInput
     v-bind="merged"
     @keydown="handleKeydown"
     ref="inputRef"
-  ></ElInput>
+    v-model="model"
+  >
+    <template v-for="(_, name) in $slots" #[name]="scope">
+      <slot :name="name" v-bind="scope" />
+    </template>
+  </ElInput>
 </template>
 
 <script setup lang="ts">
@@ -27,6 +32,7 @@ interface InputProps extends ExtractPublicPropTypes<ElInputProps> {
   preventKeyboardEvent?: boolean;
   keyboardList?: KeyboardEvent["key"][];
 }
+type InputSlots = ElInputInstance["$slots"];
 
 /**
  * * props中使用i18n
@@ -80,6 +86,46 @@ const handleKeydown = (e: KeyboardEvent | Event) => {
     emit("keydown", e);
   }
 };
+/**
+ * * Vue3.4以上才能使用 defineModel
+ */
+const [model, modifiers] = defineModel({
+  /**
+   * ! 如果为 defineModel prop 设置了一个 default 值
+   * ! 且父组件没有为该 prop 提供任何值
+   * ! 会导致父组件与子组件之间不同步。
+   */
+  default: "",
+  set(value) {
+    // nonnegative 非负整数
+    if (modifiers.nonnegative) {
+      return value.replace(/\D/g, "");
+    }
+    // decimal 小数
+    if (modifiers.decimal) {
+      // 清除数字和小数点和负号以外的字符
+      let val = value.replace(/[^\d.-]/g, "");
+      // 只保留第一个负号
+      val = val
+        .replace(/^-/, "$#$") // 仅以负号开头,才用 "$#$" 进行代替
+        .replace(/-/g, "") // 清除其他负号
+        .replace("$#$", "-"); // 换回来
+      // 不能以小数点开头
+      // value = value.replace(/^\./, "");
+      // // 只保留第一个小数点
+      // value = value
+      //   .replace(".", "$#$") // 第一个小数点以 "$#$" 进行代替
+      //   .replace(/\./g, "") // 清除其他小数点
+      //   .replace("$#$", "."); // 换回来
+      // return value;
+      // 限制小数点只能有一个，且不能在开头（除非前面有负号）
+      val = val.replace(/^\./, "").replace(/(\.\d*)\./g, "$1");
+      // 移除末尾多余的小数点（如 "123." → "123"）
+      return val.replace(/\.$/, "");
+    }
+    return value;
+  },
+});
 
 /**
  * 获取子组件的 ref
@@ -103,15 +149,20 @@ defineExpose<ElInputInstance>(
     }
   ) as ElInputInstance
 );
+/**
+ * 暴露可给父组件使用的插槽
+ */
+defineSlots<InputSlots>();
 </script>
 
 <style scoped lang="scss">
 .base-input {
   width: v-bind("inputWidth");
-  :deep(.el-input__inner) {
-    &[type="password"] {
-      // letter-spacing: -7px;
-    }
-  }
+  // :deep(.el-input__inner) {
+  //   &[type="password"] {
+  //     // -7px
+  //     letter-spacing: normal;
+  //   }
+  // }
 }
 </style>

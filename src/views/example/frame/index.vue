8447cb1fae92af7b71315b64456ef79d71d0a69b
Komorebi <2391587114@qq.com>
2025-09-16 16:51:17 +0800

    ✨ feat: 完善外部链接页面

--- src/views/example/frame/index.vue
+++ src/views/example/frame/index.vue
@@ -6,19 +6,41 @@
 -->
 <template>
   <div class="wrapper">
-    <iframe :src="frameSrc" />
+    <iframe :src="frameSrc" frameborder="0" />
   </div>
 </template>
 
 <script setup lang="ts">
 defineOptions({ name: "FrameExample" });
 const frameSrc = ref("https://element-plus.org/zh-CN/guide/design.html");
+
+// 获取父元素高度
+const parentHeight = ref("");
+const resizeObserve = new ResizeObserver((entries) => {
+  if (entries.length) {
+    const { height = 0 } = entries[0].contentRect;
+    parentHeight.value = `${height}px`;
+  }
+});
+
+onMounted(() => {
+  // 获取dom节点
+  const container = document.querySelector(
+    "#app-main .el-scrollbar__wrap"
+  ) as HTMLElement;
+  // 监听dom节点变化
+  resizeObserve.observe(container);
+});
+onUnmounted(() => {
+  resizeObserve.disconnect();
+});
 </script>
 
 <style scoped lang="scss">
 .wrapper {
   width: 100%;
-  height: 100%;
+  // height: 100%;
+  height: v-bind("parentHeight");
   iframe {
     display: block;
     box-sizing: border-box;
